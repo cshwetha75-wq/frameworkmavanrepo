d37574f9e49082a6c67c24d51ad3e51fe414a42e
cshwetha75-wq <cshwetha75@gmail.com>
2026-04-09 11:11:20 +0530

    sample mavanproject is pushing to git

---
+++ maavannproject/src/test/java/pack/ContactTest.java
@@ -0,0 +1,27 @@
+package pack;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createcontactTest() {
+		
+		String URl= System.getProperty("url");
+		String BROWSER= System.getProperty("browser","chrome");
+		String USERNAME= System.getProperty("username");
+		String PASSWORD= System.getProperty("password");
+		System.out.println(URl);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		System.out.println("execute createcontacttest");
+		
+	}
+	
+	@Test
+	public void modifycontactTest() {
+		
+		System.out.println("execute modifycontacttest");
+	}
+
+}
---
+++ maavannproject/src/test/java/pack2/OrgTest.java
@@ -0,0 +1,18 @@
+package pack2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createctOrgTest() {
+		
+		System.out.println("execute createctOrgTest");
+		
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		
+		System.out.println("execute modifyOrgtest");
+	}
+}
